8bcb2911e7f6af1f0eb5d96c80dc4608b79c6b5b
Elisabeth Shevtsova <lisabeth.shevtsova@gmail.com>
2017-03-16 13:10:44 +0400

    Update and rename Linked-Lisst.py to Linked-List.py

--- Linked-List.py
+++ Linked-List.py
@@ -1,3 +1,17 @@
+'''
+Task:
+Input Format
+
+The <i>insert function</i> has parameters: a pointer to a Node named <i>head</i>, and an integer value,<i>data</i>.
+The constructor for Node has 1 parameter: an integer value for the <i>data </i>field.
+You do not need to read anything from stdin.
+
+Output Format
+
+Your <i>insert function</i> should return a reference to the node of the linked list. 
+'''
+
+
 class Node:
     def __init__(self,data):
         self.data = data
